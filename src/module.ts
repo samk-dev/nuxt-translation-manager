79558f7c9172d0c0a16313e8c6dba8c7283fb676
Daniel Roe <daniel@roe.dev>
2024-03-26 20:25:23 +0000

    fix: don't use shadowed `resolve`

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { relative, resolve } from 'node:path'
+import { relative, resolve as pathResolve } from 'node:path'
 import { defineNuxtModule, createResolver } from '@nuxt/kit'
 import { name, version } from '../package.json'
 import generateLocales from './generateLocales'
@@ -56,7 +56,7 @@ export default defineNuxtModule<ModuleOptions>({
     }
 
     nuxt.hook('builder:watch', async (_event, path) => {
-      path = relative(nuxt.options.srcDir, resolve(nuxt.options.srcDir, path))
+      path = relative(nuxt.options.srcDir, pathResolve(nuxt.options.srcDir, path))
       if (path === csvFilePath) {
         try {
           await generateLocales(csvFileFullPath, outputDir)
